a022d730a5fb2c852c15b020aed53a0c0cd15374
EdneyPaes <edney19982010@gmail.com>
2023-01-16 16:42:04 -0300

    Usando Return

--- src/main.java
+++ src/main.java
@@ -3,10 +3,18 @@ public class main {
 
     System.out.println("Exercícios quadrilátero:");
 
-    ExercSobrecarga.area(3d);
-    ExercSobrecarga.area(5d, 5d);
-    ExercSobrecarga.area(7d,8d,9d);
-    ExercSobrecarga.area(5f, 5f);
-   
+    double areaQuadrado= ExercSobrecarga.area (3d);
+    System.out.println("Área do quadrado: " + areaQuadrado);
+
+    double areaRetangulo= ExercSobrecarga.area (5d, 5d);
+    System.out.println("Área do retângulo: " + areaRetangulo);
+
+    System.out.println("Área do retângulo: " + areaRetangulo);
+
+    double areaTrapezio= ExercSobrecarga.area (7d,8d,9d);
+    System.out.println("Área do trapézio: " + areaTrapezio);
+
   }
 }
+
+//Agora podemos guardar os valores dos retornos dos métodos em váriaveis, dessa forma abre um leque para a reutilização dos métodos.
